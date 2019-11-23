d432657c3e6707e6af5097d5cb8b2751ab1ca1e9
hodeh <hodeh@pivotal.io>
2019-11-24 02:33:06 +0400

    debug scripts

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -2,23 +2,26 @@
 set -e
 set -x
 
-M2_HOME=${HOME}/.m2
-mkdir -p ${M2_HOME}
- 
-M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
- 
-mkdir -p "${M2_LOCAL_REPO}/repository"
- 
-cat > ${M2_HOME}/settings.xml <<EOF
- 
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
-</settings>
- 
-EOF
-cd git-repo
-./mvnw -v
-./mvnw test
+export ROOT_FOLDER=$( pwd )
+export REPO=git-repo
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+GRADLE_HOME="${HOME}/.gradle"
+GRADLE_CACHE="${ROOT_FOLDER}/gradle"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+${SCRIPTS_DIR}/generate-settings.sh
+
+[[ -f "${SCRIPTS_DIR}/functions.sh" ]] && source "${SCRIPTS_DIR}/functions.sh" || \
+    echo "No functions.sh found"
+
+
+cd ${ROOT_FOLDER}/${REPO}
+testMaven "${PROJECT_TYPE}"
