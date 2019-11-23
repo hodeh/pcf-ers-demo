122c00667c90bc6207414fc4dd473a42327f8769
hodeh <hodeh@pivotal.io>
2019-11-24 03:21:06 +0400

    trigger build

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -22,6 +22,7 @@ ${SCRIPTS_DIR}/generate-settings.sh
 [[ -f "${SCRIPTS_DIR}/functions.sh" ]] && source "${SCRIPTS_DIR}/functions.sh" || \
     echo "No functions.sh found"
 
+cd ${ROOT_FOLDER}
 version=`cat version/number`
 
 cd ${ROOT_FOLDER}/${REPO}
